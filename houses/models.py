d36cee96e81f8e1bc2f52d750af854c9a2caf7ab
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-10-24 19:05:37 +0900

    #4.1 done!

--- houses/models.py
+++ houses/models.py
@@ -1,3 +1,11 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+class House(models.Model):
+
+  """Model definition"""
+
+  name = models.CharField(max_length=140)
+  price = models.PositiveIntegerField()
+  description = models.TextField()
+  address = models.CharField(max_length=140)
