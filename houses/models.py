a1304b9bbee000ca88a54d2723492288b2d02aba
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-10-30 23:10:16 +0900

    #5.1 done!

--- houses/models.py
+++ houses/models.py
@@ -1,4 +1,6 @@
+from tabnanny import verbose
 from unittest.util import _MAX_LENGTH
+from xml.etree.ElementInclude import default_loader
 from django.db import models
 
 class House(models.Model):
@@ -6,6 +8,17 @@ class House(models.Model):
   """Model definition"""
 
   name = models.CharField(max_length=140)
-  price = models.PositiveIntegerField()
+  price_per_night = models.PositiveIntegerField(
+    verbose_name= "Price",
+    help_text="Positive Number only"
+  )
   description = models.TextField()
   address = models.CharField(max_length=140)
+  pets_allowed = models.BooleanField(
+      verbose_name = "Pets Allowed?",
+      default=True,
+      help_text="Does this house allow pets?"
+      )
+
+  def __str__(self):
+    return self.name
