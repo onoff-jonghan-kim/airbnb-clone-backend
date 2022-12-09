b8b36a99b9bd35d03d831231d3b421fe5328460f
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-12-09 15:45:54 +0900

    6.10 done

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,10 @@ CUSTOM_APPS = [
     "rooms.apps.RoomsConfig",
     "experiences.apps.ExperiencesConfig",
     "categories.apps.CategoriesConfig",
+    "reviews.apps.ReviewsConfig",
+    "wishlists.apps.WishlistsConfig",
+    "bookings.apps.BookingsConfig",
+    "medias.apps.MediasConfig",
 ]
 
 SYSTEM_APPS = [
